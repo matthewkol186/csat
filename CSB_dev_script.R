4872c55c039886a626ff8404ba4ecafae0c548b6
Matthew Sun <matthewkol186@gmail.com>
2019-03-29 17:44:38 -0400

    First commit

---
+++ CSB_dev_script.R
@@ -0,0 +1,52 @@
+############################################################################
+# The followings settings are for development.  
+# Paths need to match the environment that runs this script. 
+############################################################################
+
+# set working directory and set variables to mimic call from web server
+setwd("~/Documents/RSPH/TFGH/Coverage Surevey Analysis Tool/Analysis Tool")
+
+# # configure local settings
+# source("unshared_settings.R")
+
+############################################################################
+# This file gets uploading to the web server
+source("survey_builder_v2.0.8.R")
+############################################################################
+
+############################################################################
+# Set values here in development only.  Will be sent from the web server.
+
+# csv_url<- "emory_try.csv"#"sample_KG.csv"
+# country_name<-"United States"
+# implementation_unit_header<-"Georgia"
+# drug_name<-"Ivermectin"                                                 
+# disease_name<-"Onchocerciasis"
+# number_of_subunits<-30
+# reported_coverage_header<-.7
+
+csv_url<- "mebendazole_akoko_LGA_Ondo_20190.csv"
+country_name<-"Nigeria"
+implementation_unit_header<-"Ondo"
+drug_name<-"Mebendazole"
+disease_name<-"STH"
+number_of_subunits<-30
+reported_coverage_header<-1.25
+
+############################################################################
+
+############################################################################
+# Everything below simulates running the uploaded script from the web server
+# attempt to run with variables from user sent from web server 
+
+doc<-try(wordDoc())
+
+if (inherits(doc, "try-error")) {
+        error <- conditionMessage(attr(doc, "condition"))
+        browser()
+        # call function here to swap error message with message to user
+} else {
+        error<- "none"
+        filename<-c(paste("CSBA_ResultsSummary.docx"))
+        writeDoc(doc, file=filename)
+}
---
+++ user_dev_script.R
@@ -0,0 +1,79 @@
+############################################################################
+# The followings settings are for development.  
+# Paths need to match the environment that runs this script. 
+############################################################################
+# configure local settings
+# source("unshared_settings.R")
+
+setwd("~/Documents/RSPH/TFGH/Coverage Surevey Analysis Tool/Murkonia/Version 1.1.3")
+
+############################################################################
+# This file gets uploading to the web server
+source("user_csv_survey_v1.0.10.R")
+############################################################################
+
+############################################################################
+# Set values here in development only.  Will be sent from the web server.
+
+csv_url <- "Multidistrict_Demo.csv"
+country_name<- "Murkonia"
+num_implementation_units<-3
+as.numeric(num_implementation_units)
+drug_name<-"Azithromycin"
+disease_name <- "Trachoma"
+
+implementation_unit_header<-"DISTRICT"
+cluster_header<- "VILLAGE_NUMBER"
+sex_header<- "SEX"
+age_header<-"AGE"
+#school_attendance_header<-NULL
+offered_drug_header<- "OFFERED_ALB"
+swallowed_drug_header<- "SWALLOWED_ALB"
+reported_coverage_header<- "REPORTED_COVERAGE"
+
+# csv_url <- "Multidistrict_demo_file.csv"
+# country_name<- "Murkonia"
+# num_implementation_units<-3
+# as.numeric(num_implementation_units)
+# drug_name<-"Azithromycin"
+# disease_name <- "Trachoma"
+# 
+# implementation_unit_header<-"IU"
+# cluster_header<- "Cluster"
+# sex_header<- "Sex"
+# age_header<-"Age"
+# # school_attendance_header<-NULL
+# offered_drug_header<- "Drug_Offer"
+# swallowed_drug_header<- "Drug_Swallow"
+# reported_coverage_header<- "Reported_Coverage"
+
+# csv_url <- "Murkonia_Data.csv"
+# country_name <- "Murkonia"
+# num_implementation_units <-21
+# drug_name <- "Ivermectin"
+# disease_name <- "Onchocerciasis"
+# 
+# implementation_unit_header <- "DIST"
+# cluster_header <- "CLUSTER"
+# sex_header <-"SEY"
+# age_header <-"AGE"
+# #school_attendance_header<-
+# offered_drug_header <-"Q201_ALB"
+# swallowed_drug_header <-"Q201_DEC"
+# reported_coverage_header <-"r_coverage"
+
+############################################################################
+
+############################################################################
+# Everything below simulates running the uploaded script from the web server
+# attempt to run with variables from user sent from web server 
+doc<-try(wordDoc())
+
+if (inherits(doc, "try-error")) {
+        error <- conditionMessage(attr(doc, "condition"))
+        # call function here to swap error message with message to user
+} else {
+        error<- "none"
+        filename<-c(paste("CSA_ResultsSummary.docx"))
+        writeDoc(doc, file=filename)
+}
