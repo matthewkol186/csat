3d24351e402f875011aa6fda1c9440a7be317b19
Matthew Sun <matthewkol186@gmail.com>
2019-04-15 08:14:58 -0700

    Revert "threshold assignment function and note on error message for user during upload"
    This reverts commit 702437976d00b6fc2e44f6e7c97d4625ecd14856, reversing
    changes made to 3fc72a3d6c973d0e4624b44c1a884cff14e945bd.

--- R/check_data_validity.R
+++ R/check_data_validity.R
@@ -1,6 +1,5 @@
-
-
 # Check whether the disease matches the drug administered.
+
 checkDiseaseDrugMatch <- function(disease, drug) {
   match = switch(disease,
                  "Lymphatic Filariasis" = drug=="Ivermectin" | drug=="Albendazole" | drug=="ALB & IVM" | drug=="ALB & DEC" | drug=="ALB, DEC, & IVM",
@@ -10,13 +9,4 @@ checkDiseaseDrugMatch <- function(disease, drug) {
                  "Schistosomiasis" = drug=="Praziquantel"
   )
   return(match)
-}
-
-#set threshold value (desired level of population coverage) based on WHO recommendation
-setDiseaseThreshold <- function(disease) {
-  if (disease %in% c("Onchocerciasis", "Lymphatic Filariasis")) {
-    thresh = 65
-  } else if (disease == "Trachoma") {
-    thresh = 80
-  } else thresh = 75 #Value for STH and Schistosomiasis
 }
